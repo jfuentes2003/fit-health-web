9b9099c760c2a42602f52ed28aa7abeed3dfa020
jfuentes2003 <jfuentes1@guilford.edu>
2024-03-28 14:41:20 -0400

    Main Page Done
    Everything in Page #1 is done

---
+++ pageconfig.js
@@ -0,0 +1,223 @@
+// Modern fitness equipment pic title"
+var mfeParagraph = document.getElementById("MFE");
+
+ {
+    // Change font color to red
+    mfeParagraph.style.color = "darkred";
+    
+    // Change font size to 20 pixels
+    mfeParagraph.style.fontSize = "22px";
+    mfeParagraph.style.fontWeight = "bold";
+}
+
+// Modern fitness equipment paragraph in picture"
+var ppParagraph = document.getElementById("PP");
+
+// Check if the element exists
+if (ppParagraph) {
+    // Change font color to blue
+    ppParagraph.style.color = "white";
+    
+    // Change font size to 18 pixels
+    ppParagraph.style.fontSize = "16px";
+} 
+
+
+
+
+
+
+
+//Group Exercises Classes
+// Group exercise classes title
+var geParagraph = document.getElementById("GE");
+
+if (geParagraph) {
+    // Change font color to red
+    geParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    geParagraph.style.fontSize = "22px";
+    geParagraph.style.fontWeight = "bold";
+}
+
+// Group exercise classes paragraph
+var gpParagraph = document.getElementById("GP");
+
+if (gpParagraph) {
+    // Change font color to blue
+    gpParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    gpParagraph.style.fontSize = "16px";
+}
+
+
+
+
+
+//Personal Trainig Sessions
+// Personal training sessions title
+var ptParagraph = document.getElementById("PT");
+
+if (ptParagraph) {
+    // Change font color to red
+    ptParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    ptParagraph.style.fontSize = "22px";
+    ptParagraph.style.fontWeight = "bold";
+}
+
+// Personal training sessions paragraph
+var psParagraph = document.getElementById("PS");
+
+if (psParagraph) {
+    // Change font color to blue
+    psParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    psParagraph.style.fontSize = "16px";
+}
+
+
+
+
+
+//Cardia Area//
+// Cardio area title
+var caParagraph = document.getElementById("CA");
+
+if (caParagraph) {
+    // Change font color to red
+    caParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    caParagraph.style.fontSize = "22px";
+    caParagraph.style.fontWeight = "bold";
+}
+
+// Cardio area paragraph
+var cpParagraph = document.getElementById("CP");
+
+if (cpParagraph) {
+    // Change font color to blue
+    cpParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    cpParagraph.style.fontSize = "16px";
+}
+
+
+
+
+
+
+//Weightlifting Area//
+// Weightlifting Area title
+var waParagraph = document.getElementById("WA");
+
+if (waParagraph) {
+    // Change font color to red
+    waParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    waParagraph.style.fontSize = "22px";
+    waParagraph.style.fontWeight = "bold";
+}
+
+// Weightlifting Area paragraph
+var wpParagraph = document.getElementById("WP");
+
+if (wpParagraph) {
+    // Change font color to blue
+    wpParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    wpParagraph.style.fontSize = "16px";
+}
+
+
+
+//Indoor Pool 
+// Indoor Pool title
+var itParagraph = document.getElementById("It");
+
+if (itParagraph) {
+    // Change font color to red
+    itParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    itParagraph.style.fontSize = "22px";
+    itParagraph.style.fontWeight = "bold";
+}
+
+// Indoor Pool paragraph
+var ipParagraph = document.getElementById("IP");
+
+if (ipParagraph) {
+    // Change font color to blue
+    ipParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    ipParagraph.style.fontSize = "16px";
+}
+
+
+
+
+
+
+
+//Sauna and Steam Room
+// Sauna & Steam Room title
+var stParagraph = document.getElementById("ST");
+
+if (stParagraph) {
+    // Change font color to red
+    stParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    stParagraph.style.fontSize = "22px";
+    stParagraph.style.fontWeight = "bold";
+}
+
+// Sauna & Steam Room paragraph
+var spParagraph = document.getElementById("SP");
+
+if (spParagraph) {
+    // Change font color to blue
+    spParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    spParagraph.style.fontSize = "16px";
+}
+
+
+
+
+
+//Nutritional Counseling
+// Nutritional Counseling title
+var ntParagraph = document.getElementById("NT");
+
+if (ntParagraph) {
+    // Change font color to red
+    ntParagraph.style.color = "darkred";
+    
+    // Change font size to 22 pixels
+    ntParagraph.style.fontSize = "22px";
+    ntParagraph.style.fontWeight = "bold";
+}
+
+// Nutritional Counseling paragraph
+var npParagraph = document.getElementById("NP");
+
+if (npParagraph) {
+    // Change font color to blue
+    npParagraph.style.color = "white";
+    
+    // Change font size to 16 pixels
+    npParagraph.style.fontSize = "16px";
+}
+
